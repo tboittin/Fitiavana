4f1e31bc79219c1c42722822e2d8db9bc32e9c8c
Thomas Boittin <tboittin@gmail.com>
2022-11-23 20:43:17 +0100

    firebase init + auth + user db

--- services/firebaseInit/index.js
+++ services/firebaseInit/index.js
@@ -1,6 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth"
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +29,105 @@ const firebaseConfig = {
   storageBucket: "fitiavana-website.appspot.com",
   messagingSenderId: "327000185750",
   appId: "1:327000185750:web:672b8b226f942c15ff5bb7",
-  measurementId: "G-2ZSK3L25Q8"
-};
+  measurementId: "G-2ZSK3L25Q8",
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then(userCredential => {
+//     // Signed in
+//     const user = userCredential.user
+//     // ...
+//   })
+//   .catch(error => {
+//     const errorCode = error.code
+//     const errorMessage = error.message
+//     // ..
+//   })
+// signInWithEmailAndPassword(auth, email, password)
+//   .then(userCredential => {
+//     // Signed in
+//     const user = userCredential.user
+//     // ...
+//   })
+//   .catch(error => {
+//     const errorCode = error.code
+//     const errorMessage = error.message
+//   })
+
+const googleProvider = new GoogleAuthProvider()
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider)
+    const user = res.user
+    const q = query(collection(db, "users"), where("uid", "==", user.uid))
+    const docs = await getDocs(q)
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password)
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    })
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+const sendPasswordReset = async email => {
+  try {
+    await sendPasswordResetEmail(auth, email)
+    alert("Password reset link sent!")
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+const logout = () => {
+  signOut(auth)
+}
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+}
