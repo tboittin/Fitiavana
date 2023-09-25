85d7f93752d8c9e75fc659e85ec9f801e30edb0f
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2023-09-25 08:33:51 +0000

    fix mobile navbar link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react"
-import Link from "next/link"
+import React, { useState } from 'react'
+import Link from 'next/link'
 
 // import Image from "next/image"
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-import styles from "../styles/Navbar.module.scss"
-import { navbarText } from "../wording/wording"
+import styles from '../styles/Navbar.module.scss'
+import { navbarText } from '../wording/wording'
 
 export default function Navbar() {
     const [displayNavbar, setDisplayNavbar] = useState(false)
@@ -18,12 +18,12 @@ export default function Navbar() {
 
     return (
         <div className={styles.navbar}>
-            <Link href='/'>
-                <div className={styles.navbarLogo} >
+            <Link href="/">
+                <div className={styles.navbarLogo}>
                     {/* replace img with Image from next */}
                     <img
-                        src='/images/logo/290538622_1121932522038336_2469259896100467841_n.jpg'
-                        alt='fitiavana-logo'
+                        src="/images/logo/290538622_1121932522038336_2469259896100467841_n.jpg"
+                        alt="fitiavana-logo"
                     />
                     <span className={styles.navbarLogoTitle}>
                         - Association Fitiavana
@@ -37,8 +37,8 @@ export default function Navbar() {
             </div>
             <div className={styles.desktop}>
                 <ul className={styles.navbarLinksContainer}>
-                    {navbarText.map(subject => (
-                        <div key={subject.title} className='m-2'>
+                    {navbarText.map((subject) => (
+                        <div key={subject.title} className="m-2">
                             <Link href={subject.link}>
                                 <a className={styles.navbarLink}>
                                     {subject.title}
@@ -50,9 +50,13 @@ export default function Navbar() {
             </div>
             {displayNavbar && (
                 <div className={styles.mobileMenu}>
-                    {navbarText.map(subject => (
+                    {navbarText.map((subject) => (
                         <div key={subject.title} className={styles.navbarLink}>
-                            {subject.title}
+                            <Link href={subject.link}>
+                                <a className={styles.navbarLink}>
+                                    {subject.title}
+                                </a>
+                            </Link>
                         </div>
                     ))}
                 </div>
