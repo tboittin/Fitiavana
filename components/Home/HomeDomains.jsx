b378f38d0deee24fb97cb4621290273030b45387
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2023-10-07 05:15:03 +0000

    domains images

--- components/Home/HomeDomains.jsx
+++ components/Home/HomeDomains.jsx
@@ -5,7 +5,7 @@ const HomeDomains = () => {
         {
             title: "Scolarité",
             photoUrl:
-                "https://images.unsplash.com/photo-1588072432836-e10032774350?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+                "https://static.blog4ever.com/2008/01/171408/artfichier_171408_5458734_201602054409760.jpg",
             photoDescription: "Des enfants à l'école.",
             text: [
                 "Incididunt laborum deserunt eiusmod aliqua consectetur ex elit sunt culpa eiusmod veniam in mollit. Ut ad minim amet sit eiusmod anim cillum dolor eiusmod. Voluptate sunt eu quis enim voluptate cillum irure tempor consectetur. Eiusmod minim in nisi anim labore mollit laborum nulla ex ea exercitation amet laborum labore.",
@@ -15,7 +15,7 @@ const HomeDomains = () => {
         {
             title: "Alimentaire",
             photoUrl:
-                "https://images.unsplash.com/photo-1551218372-a8789b81b253?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                "https://static.blog4ever.com/2008/01/171408/big_artfichier_171408_8530137_20200310153197.jpg",
             photoDescription: "Des enfants qui mangent.",
             text: [
                 "Incididunt laborum deserunt eiusmod aliqua consectetur ex elit sunt culpa eiusmod veniam in mollit. Ut ad minim amet sit eiusmod anim cillum dolor eiusmod. Voluptate sunt eu quis enim voluptate cillum irure tempor consectetur. Eiusmod minim in nisi anim labore mollit laborum nulla ex ea exercitation amet laborum labore.",
