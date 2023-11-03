f3bee9b8f0feb92decc5e6527db55a849c5b4869
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2023-11-03 22:31:32 +0000

    parrainage spacing video

--- components/Parrainage/ParrainagePresentation.jsx
+++ components/Parrainage/ParrainagePresentation.jsx
@@ -10,8 +10,8 @@ const ParrainagePresentation = () => {
             <h2 className='bg-blue-theme text-white text-4xl h-45vh w-full flex justify-center p-6'>
                 Pourquoi Parrainer ?
             </h2>
-            <div className='items-center mx-auto'>
-                <div className="mx-auto lg:w-8/12 ">
+            <div className='items-center mx-auto lg:mt-[10vh]'>
+                <div className="mx-auto lg:w-8/12 mt-5 ">
                     <VideoSection title='' id={videoInfos.urlYoutubeLink}/>
                 </div>
             </div>
